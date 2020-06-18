b32eb13ad7d796409d8a6f035bf611302492e4b2
Aaron Hathaway <46059772+aaronhathaway1@users.noreply.github.com>
2020-06-18 11:17:20 -0500

    team activity week 9

--- week04/scripts/teamActivity.js
+++ week04/scripts/teamActivity.js
@@ -5,71 +5,67 @@ squares.forEach(element => {
 
 let count = 0;
 
-function playerTurn(event){
-    if(count % 2 == 0){
+function playerTurn(event) {
+    if (count % 2 == 0) {
         event.target.innerHTML = "X";
         document.getElementById("message").innerHTML = "Player 2's turn";
-    } else{
+    } else {
         event.target.innerHTML = "O";
         document.getElementById("message").innerHTML = "Player 1's turn";
     }
     event.target.removeEventListener("touchend", playerTurn);
     count++;
 
-    if(checkWinner(){
+    if (checkWinner() {
 
-        //Check winner
-        checkTie();
-    }
-    
-}
+            //Check winner
+            checkTie();
+        }
 
-function resetBoard() {
-    squares.forEach(element => {
-        element.innerHTML = "";
-        element.addEventListener("touchend", playerTurn);
-    })
-    count = 0;
-    document.getElementById("message").innerHTML
-}
+        function resetBoard() {
+            squares.forEach(element => {
+                element.innerHTML = "";
+                element.addEventListener("touchend", playerTurn);
+            })
+            count = 0;
+            document.getElementById("message").innerHTML
+        }
 
-function checkTie(){
-    if (count == 9){
-        document.getElementById("message").innerHTML = "You tied";
-    }
-}
+        function checkTie() {
+            if (count == 9) {
+                document.getElementById("message").innerHTML = "You tied";
+            }
+        }
 
-function checkWinner(){
-    if ((squares[0].innerHTML == "X" && squares[1].innerHTML == "X" && squares[2].innerHTML == "X")
-    || (squares[0].innerHTML == "X" && squares[4].innerHTML == "X" && squares[8].innerHTML == "X")
-    || (squares[0].innerHTML == "X" && squares[3].innerHTML == "X" && squares[6].innerHTML == "X")
-    || (squares[1].innerHTML == "X" && squares[4].innerHTML == "X" && squares[7].innerHTML == "X")
-    || (squares[3].innerHTML == "X" && squares[4].innerHTML == "X" && squares[5].innerHTML == "X")
-    || (squares[2].innerHTML == "X" && squares[4].innerHTML == "X" && squares[6].innerHTML == "X")
-    || (squares[2].innerHTML == "X" && squares[5].innerHTML == "X" && squares[8].innerHTML == "X")
-    || (squares[6].innerHTML == "X" && squares[7].innerHTML == "X" && squares[8].innerHTML == "X")){
-        document.getElementById("message").innerHTML = "Player 1 wins";
-        removeAllEventListeners();
-        return true;
-    } else if ((squares[0].innerHTML == "X" && squares[1].innerHTML == "X" && squares[2].innerHTML == "X")
-    || (squares[0].innerHTML == "O" && squares[4].innerHTML == "O" && squares[8].innerHTML == "O")
-    || (squares[0].innerHTML == "O" && squares[3].innerHTML == "O" && squares[6].innerHTML == "O")
-    || (squares[1].innerHTML == "O" && squares[4].innerHTML == "O" && squares[7].innerHTML == "O")
-    || (squares[3].innerHTML == "O" && squares[4].innerHTML == "O" && squares[5].innerHTML == "O")
-    || (squares[2].innerHTML == "O" && squares[4].innerHTML == "O" && squares[6].innerHTML == "O")
-    || (squares[2].innerHTML == "O" && squares[5].innerHTML == "O" && squares[8].innerHTML == "O")
-    || (squares[6].innerHTML == "O" && squares[7].innerHTML == "O" && squares[8].innerHTML == "O")){
-        document.getElementById("message").innerHTML = "Player 2 win";;
-        return true;
-    }  else {
-        return false;
-    } 
-}
+        function checkWinner() {
+            if ((squares[0].innerHTML == "X" && squares[1].innerHTML == "X" && squares[2].innerHTML == "X") ||
+                (squares[0].innerHTML == "X" && squares[4].innerHTML == "X" && squares[8].innerHTML == "X") ||
+                (squares[0].innerHTML == "X" && squares[3].innerHTML == "X" && squares[6].innerHTML == "X") ||
+                (squares[1].innerHTML == "X" && squares[4].innerHTML == "X" && squares[7].innerHTML == "X") ||
+                (squares[3].innerHTML == "X" && squares[4].innerHTML == "X" && squares[5].innerHTML == "X") ||
+                (squares[2].innerHTML == "X" && squares[4].innerHTML == "X" && squares[6].innerHTML == "X") ||
+                (squares[2].innerHTML == "X" && squares[5].innerHTML == "X" && squares[8].innerHTML == "X") ||
+                (squares[6].innerHTML == "X" && squares[7].innerHTML == "X" && squares[8].innerHTML == "X")) {
+                document.getElementById("message").innerHTML = "Player 1 wins";
+                removeAllEventListeners();
+                return true;
+            } else if ((squares[0].innerHTML == "X" && squares[1].innerHTML == "X" && squares[2].innerHTML == "X") ||
+                (squares[0].innerHTML == "O" && squares[4].innerHTML == "O" && squares[8].innerHTML == "O") ||
+                (squares[0].innerHTML == "O" && squares[3].innerHTML == "O" && squares[6].innerHTML == "O") ||
+                (squares[1].innerHTML == "O" && squares[4].innerHTML == "O" && squares[7].innerHTML == "O") ||
+                (squares[3].innerHTML == "O" && squares[4].innerHTML == "O" && squares[5].innerHTML == "O") ||
+                (squares[2].innerHTML == "O" && squares[4].innerHTML == "O" && squares[6].innerHTML == "O") ||
+                (squares[2].innerHTML == "O" && squares[5].innerHTML == "O" && squares[8].innerHTML == "O") ||
+                (squares[6].innerHTML == "O" && squares[7].innerHTML == "O" && squares[8].innerHTML == "O")) {
+                document.getElementById("message").innerHTML = "Player 2 win";;
+                return true;
+            } else {
+                return false;
+            }
+        }
 
-
-
-function removeAllEventListeners() {
-    squares.forEach(element => {
-        element.removeEventListener("touchend", playerTurn);
-    })
-}
+        function removeAllEventListeners() {
+            squares.forEach(element => {
+                element.removeEventListener("touchend", playerTurn);
+            })
+        }
